81cab0db06311a0ce489b5ac7dcc4736f1ec07f2
shooooooma415 <shoma20040415@icloud.com>
2025-04-25 14:55:58 +0900

    feat: Mongo接続

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,36 @@
 package main
 
 import (
+	"context"
 	"net/http"
+	"os"
+
 	"github.com/labstack/echo/v4"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
+	uri := os.Getenv("MONGO_URI")
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+
+	db := client.Database("chikokulympic_prod")
+	collection := db.Collection("hoge")
+
 	e := echo.New()
 
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, Echo!")
+		var result bson.M
+		err := collection.FindOne(context.TODO(), bson.M{}).Decode(&result)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
+		}
+
+		return c.JSON(http.StatusOK, result)
 	})
 
 	e.Start(":8080")
