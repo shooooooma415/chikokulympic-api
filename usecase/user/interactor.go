c94e081e6ea5971a462da4929991470824d2282b
shooooooma415 <shoma20040415@icloud.com>
2025-04-29 21:41:12 +0900

    feat: interactor.goの実装

--- usecase/user/interactor.go
+++ usecase/user/interactor.go
@@ -1,1 +1,26 @@
 package user
+
+import (
+	"chikokulympic-api/domain/entity"
+	"chikokulympic-api/domain/repository"
+)
+
+type userUsecase struct {
+	userRepo repository.UserRepository
+}
+
+func NewUserUsecase(r repository.UserRepository) UserUsecase {
+	return &userUsecase{userRepo: r}
+}
+
+func (u *userUsecase) RegisterUser(user *entity.User) (*entity.User, error) {
+	return u.userRepo.CreateUser(user)
+}
+
+func (u *userUsecase) AuthenticateUser(authID entity.AuthID) (*entity.User, error) {
+	return u.userRepo.FindUserByAuthID(authID)
+}
+
+func (u *userUsecase) UpdateUserName(user *entity.User) (*entity.User, error) {
+	return u.userRepo.UpdateUser(user)
+}
