9605c429e6629122e0de2c1951e41d3f4850989c
shooooooma415 <shoma20040415@icloud.com>
2025-04-29 11:50:51 +0900

    feat: group.goに構造体を追加

--- domain/group/group.go
+++ domain/group/group.go
@@ -1,1 +1,18 @@
 package group
+
+import (
+	"chikokulympic-api/domain/user"
+)
+
+type GroupID string
+type GroupName string
+type GroupDescription string
+
+type GroupMembers []user.UserID
+
+type Group struct {
+	GroupID          GroupID
+	GroupName        GroupName
+	GroupDescription GroupDescription
+	GroupMembers     GroupMembers
+}
