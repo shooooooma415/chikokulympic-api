e5a93f5bfb9981c2242a93c6a0beca19a38320a3
shooooooma415 <shoma20040415@icloud.com>
2025-04-29 21:16:35 +0900

    feat: interfaceの追加

--- usecase/user/usecase.go
+++ usecase/user/usecase.go
@@ -1,3 +1,9 @@
 package user
 
+import "chikokulympic-api/domain/entity"
 
+type UserUsecase interface {
+	RegisterUser(user *entity.User) (*entity.User, error)
+	AuthenticateUser(authID entity.AuthID) (*entity.User, error)
+	UpdateUserName(user *entity.User) (*entity.User, error)
+}
