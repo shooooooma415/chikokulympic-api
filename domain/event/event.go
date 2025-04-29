bf8eb64ef490dec19466b9b69bc2489955bb64b9
shooooooma415 <shoma20040415@icloud.com>
2025-04-29 13:27:08 +0900

    feat: 構造体の追加

--- domain/event/event.go
+++ domain/event/event.go
@@ -1,13 +1,32 @@
 package event
 
 import (
+	"chikokulympic-api/domain/location"
 	"chikokulympic-api/domain/user"
+	"time"
 )
 
 type EventID string
 type EventTitle string
 type EventDescription string
 type LocationName string
-type EventCost int
+type Cost int
 type EventMessage string
-type EventAuthorID user.UserID
+type StartDateTIme time.Time
+type EndDateTime time.Time
+type EventClosingDateTime time.Time
+
+type Event struct {
+	EventID              EventID
+	EventTitle           EventTitle
+	EventDescription     EventDescription
+	EventLocationName    LocationName
+	Cost                 Cost
+	EventMessage         EventMessage
+	EventAuthorID        user.UserID
+	Latitude             location.Latitude
+	Longitude            location.Longitude
+	EventStartDateTime   StartDateTIme
+	EventEndDateTime     EndDateTime
+	EventClosingDateTime EventClosingDateTime
+}
