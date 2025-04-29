25d2f7c52d8b37ede5adbccb6da44ad3b0a831b2
shooooooma415 <shoma20040415@icloud.com>
2025-04-29 13:56:53 +0900

    feat: interfaceの実装

--- domain/group/group.go
+++ domain/group/group.go
@@ -1,6 +1,7 @@
 package group
 
 import (
+	"chikokulympic-api/domain/event"
 	"chikokulympic-api/domain/user"
 )
 
@@ -9,10 +10,12 @@ type GroupName string
 type GroupDescription string
 
 type GroupMembers []user.UserID
+type GroupEvents []event.EventID
 
 type Group struct {
 	GroupID          GroupID
 	GroupName        GroupName
 	GroupDescription GroupDescription
 	GroupMembers     GroupMembers
+	GroupEvents      GroupEvents
 }
--- domain/group/repo.go
+++ domain/group/repo.go
@@ -1,1 +1,8 @@
 package group
+
+type GroupRepository interface {
+	FindGroupByGroupID(GroupID GroupID) (*Group, error)
+	CreateGroup(Group *Group) (*Group, error)
+	DeleteGroup(Group *Group) (*Group, error)
+	UpdateGroup(Group *Group) (*Group, error)
+}
