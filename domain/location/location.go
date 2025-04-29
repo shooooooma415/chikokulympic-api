60cfb5f462450b776a3a3e3dc61697bc0b5e2348
shooooooma415 <shoma20040415@icloud.com>
2025-04-29 12:11:09 +0900

    feat: 構造体を追加

--- domain/location/location.go
+++ domain/location/location.go
@@ -1,1 +1,12 @@
 package location
+
+import user "chikokulympic-api/domain/auth"
+
+type Latitude float64
+type Longitude float64
+
+type UserLocation struct {
+	UserID    user.UserID
+	Latitude  Latitude  `json:"latitude"`
+	Longitude Longitude `json:"longitude"`
+}
