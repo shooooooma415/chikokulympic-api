a03398de370a3197a39e66cd52804124a897024d
shooooooma415 <shoma20040415@icloud.com>
2025-04-29 12:06:16 +0900

    feat: 構造体を追加

--- domain/event/event.go
+++ domain/event/event.go
@@ -1,1 +1,13 @@
 package event
+
+import (
+	"chikokulympic-api/domain/user"
+)
+
+type EventID string
+type EventTitle string
+type EventDescription string
+type LocationName string
+type EventCost int
+type EventMessage string
+type EventAuthorID user.UserID
