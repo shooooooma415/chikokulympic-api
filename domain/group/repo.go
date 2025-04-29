c9fe5bd40c44c38e794a22032210218478025926
shooooooma415 <shoma20040415@icloud.com>
2025-04-29 14:02:03 +0900

    feat: interfaceの追加

--- domain/group/repo.go
+++ domain/group/repo.go
@@ -1,7 +1,10 @@
 package group
 
+import "chikokulympic-api/domain/user"
+
 type GroupRepository interface {
 	FindGroupByGroupID(GroupID GroupID) (*Group, error)
+	FindGroupByUserID(UserID user.UserID) (*Group, error)
 	CreateGroup(Group *Group) (*Group, error)
 	DeleteGroup(Group *Group) (*Group, error)
 	UpdateGroup(Group *Group) (*Group, error)
