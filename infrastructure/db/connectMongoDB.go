9fe409e39a8b486f565da5f2e51be3942a5e377d
shooooooma415 <shoma20040415@icloud.com>
2025-05-02 07:42:32 +0900

    feat: connectMongoDB.goの追加

---
+++ infrastructure/db/connectMongoDB.go
@@ -0,0 +1,69 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+type MongoConfig struct {
+	URI      string
+	Database string
+}
+
+func NewMongoConfig() *MongoConfig {
+	uri := os.Getenv("MONGODB_URI")
+	database := os.Getenv("MONGODB_DATABASE")
+
+	return &MongoConfig{
+		URI:      uri,
+		Database: database,
+	}
+}
+
+func ConnectMongoDB(config *MongoConfig) (*mongo.Database, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	clientOptions := options.Client().ApplyURI(config.URI)
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create MongoDB client: %w", err)
+	}
+
+	if err := client.Ping(ctx, readpref.Primary()); err != nil {
+		return nil, fmt.Errorf("failed to ping MongoDB: %w", err)
+	}
+
+	log.Printf("Connected to MongoDB at %s", config.URI)
+	return client.Database(config.Database), nil
+}
+
+func DisconnectMongoDB(client *mongo.Client) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := client.Disconnect(ctx); err != nil {
+		return fmt.Errorf("failed to disconnect MongoDB client: %w", err)
+	}
+
+	log.Println("Disconnected from MongoDB")
+	return nil
+}
+
+func GetMongoDBConnection() (*mongo.Database, *mongo.Client, error) {
+	config := NewMongoConfig()
+	db, err := ConnectMongoDB(config)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	client := db.Client()
+	return db, client, nil
+}
