b4367b383eda7a93f01f65d0f0df513d167c3186
shooooooma415 <shoma20040415@icloud.com>
2025-05-03 17:43:14 +0900

    feat: CreateGroupUseCaseの実装

--- usecase/createGroup.go
+++ usecase/createGroup.go
@@ -1,1 +1,30 @@
 package usecase
+
+import (
+	"chikokulympic-api/domain/entity"
+	"chikokulympic-api/domain/repository"
+)
+
+type CreateGroupUseCase interface {
+	Execute(groupName string, groupPassword string, groupManagerID string, groupDescription string) (*entity.Group, error)
+}
+
+type CreateGroupUseCaseImpl struct {
+	groupRepo repository.GroupRepository
+}
+
+func NewCreateGroupUseCase(groupRepo repository.GroupRepository) *CreateGroupUseCaseImpl {
+	return &CreateGroupUseCaseImpl{
+		groupRepo: groupRepo,
+	}
+}
+func (uc *CreateGroupUseCaseImpl) Execute(groupName string, groupPassword string, groupManagerID string, groupDescription string) (*entity.Group, error) {
+	group := &entity.Group{
+		GroupName:        entity.GroupName(groupName),
+		GroupPassword:    entity.GroupPassword(groupPassword),
+		GroupManagerID:   entity.UserID(groupManagerID),
+		GroupDescription: entity.GroupDescription(groupDescription),
+	}
+
+	return uc.groupRepo.CreateGroup(group)
+}
