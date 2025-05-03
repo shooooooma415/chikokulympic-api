0682e7b37bdb4fd9b20cfab257817676d1b810d5
shooooooma415 <shoma20040415@icloud.com>
2025-05-03 15:39:57 +0900

    hotfix: testUtilsの仕様を変更

--- infrastructure/mongo/repository/testUtils/testUtils.go
+++ infrastructure/mongo/repository/testUtils/testUtils.go
@@ -2,6 +2,8 @@ package testUtils
 
 import (
 	"context"
+	"os"
+	"path/filepath"
 	"testing"
 
 	mongoDB "chikokulympic-api/infrastructure/mongo"
@@ -9,9 +11,34 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-// SetupTestDB はテスト用のMongoDBデータベースを準備し、クリーンアップ関数を返します
+func findProjectRoot() (string, error) {
+	dir, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+
+	for {
+		if _, err := os.Stat(filepath.Join(dir, "go.mod")); err == nil {
+			return dir, nil
+		}
+
+		parentDir := filepath.Dir(dir)
+		if parentDir == dir {
+			return "", os.ErrNotExist
+		}
+		dir = parentDir
+	}
+}
+
 func SetupTestDB(t *testing.T) (*mongo.Database, func()) {
-	db, client, err := mongoDB.GetMongoDBConnectionWithEnvFile(".env.local")
+	rootDir, err := findProjectRoot()
+	if err != nil {
+		t.Fatalf("Failed to find project root: %v", err)
+	}
+
+	envPath := filepath.Join(rootDir, ".env.local")
+
+	db, client, err := mongoDB.GetMongoDBConnectionWithEnvFile(envPath)
 	if err != nil {
 		t.Fatalf("Failed to connect to MongoDB: %v", err)
 	}
