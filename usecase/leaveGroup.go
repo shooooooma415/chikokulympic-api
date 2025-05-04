433cf30fb5ee5567fbc35cf6a69854b273970583
shooooooma415 <shoma20040415@icloud.com>
2025-05-04 09:57:03 +0900

    feat: leaveGroupの実装

--- usecase/leaveGroup.go
+++ usecase/leaveGroup.go
@@ -1,1 +1,45 @@
 package usecase
+
+import (
+	"chikokulympic-api/domain/entity"
+	"chikokulympic-api/domain/repository"
+	"fmt"
+)
+
+type LeaveGroupUseCase interface {
+	Execute() error
+}
+
+type LeaveGroupUseCaseImpl struct {
+	groupRepo repository.GroupRepository
+	userID    entity.UserID
+	group     entity.Group
+}
+
+func NewLeaveGroupUseCase(groupRepo repository.GroupRepository, userID entity.UserID, group entity.Group) *LeaveGroupUseCaseImpl {
+	return &LeaveGroupUseCaseImpl{
+		groupRepo: groupRepo,
+		userID:    userID,
+		group:     group,
+	}
+}
+
+func (uc *LeaveGroupUseCaseImpl) Execute() error {
+	groupFound, err := uc.groupRepo.FindGroupByGroupName(&uc.group.GroupName)
+	if err != nil {
+		return err
+	}
+	if groupFound == nil {
+		return fmt.Errorf("グループが見つかりません")
+	}
+
+	for i, memberID := range groupFound.GroupMembers {
+		if memberID == uc.userID {
+			groupFound.GroupMembers = append(groupFound.GroupMembers[:i], groupFound.GroupMembers[i+1:]...)
+			break
+		}
+	}
+
+	_, err = uc.groupRepo.UpdateGroup(groupFound)
+	return err
+}
