e26a81fdd92cec194884d9052e9ae1ec2f024266
shooooooma415 <shoma20040415@icloud.com>
2025-05-07 21:56:13 +0900

    hotfix: グループ作成時にmanagerIDがUsersに存在するかチェック

--- usecase/createGroup.go
+++ usecase/createGroup.go
@@ -12,17 +12,28 @@ type CreateGroupUseCase interface {
 
 type CreateGroupUseCaseImpl struct {
 	groupRepo repository.GroupRepository
+	userRepo  repository.UserRepository
 	group     *entity.Group
 }
 
-func NewCreateGroupUseCase(groupRepo repository.GroupRepository, group *entity.Group) *CreateGroupUseCaseImpl {
+func NewCreateGroupUseCase(groupRepo repository.GroupRepository, userRepo repository.UserRepository, group *entity.Group) *CreateGroupUseCaseImpl {
 	return &CreateGroupUseCaseImpl{
 		groupRepo: groupRepo,
+		userRepo:  userRepo,
 		group:     group,
 	}
 }
 
 func (uc *CreateGroupUseCaseImpl) Execute() (*entity.Group, error) {
+	user, err := uc.userRepo.FindUserByUserID(uc.group.GroupManagerID)
+	if err != nil {
+		return nil, fmt.Errorf("ユーザーの取得に失敗しました: %v", err)
+	}
+	if user == nil {
+		return nil, fmt.Errorf("指定されたユーザーID %s が存在しません", err)
+	}
+
+	// グループ名の重複チェック
 	existingGroup, err := uc.groupRepo.FindGroupByGroupName(&uc.group.GroupName)
 	if err == nil && existingGroup != nil {
 		return nil, fmt.Errorf("グループ名 '%s' は既に使用されています", string(uc.group.GroupName))
