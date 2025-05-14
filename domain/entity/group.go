8a936feacf62c828b5bc4f4fef40e138f8885024
shooooooma415 <shoma20040415@icloud.com>
2025-05-15 00:17:42 +0900

    hotfix: 余計なコメントの削除

--- domain/entity/group.go
+++ domain/entity/group.go
@@ -1,31 +1,17 @@
 package entity
 
-// GroupID はグループIDの型定義
-// @Description グループを一意に識別するID
 type GroupID string
 
-// GroupName はグループ名の型定義
-// @Description グループの名前
 type GroupName string
 
-// GroupPassword はグループパスワードの型定義
-// @Description グループへの参加に必要なパスワード
 type GroupPassword string
 
-// GroupDescription はグループ説明の型定義
-// @Description グループの説明文
 type GroupDescription string
 
-// GroupMembers はグループメンバーのリスト型定義
-// @Description グループに所属するメンバーのIDリスト
 type GroupMembers []UserID
 
-// GroupEvents はグループイベントのリスト型定義
-// @Description グループに関連するイベントのIDリスト
 type GroupEvents []EventID
 
-// Group はグループエンティティの構造体
-// @Description グループエンティティ
 type Group struct {
 	GroupID          GroupID          `bson:"group_id" json:"group_id" example:"group123"`
 	GroupName        GroupName        `bson:"name" json:"group_name" example:"テストグループ"`
