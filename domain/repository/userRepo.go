ba690c2d179930dd2f392f4c7cae3af7eb7c0af0
shooooooma415 <shoma20040415@icloud.com>
2025-05-20 17:48:33 +0900

    hotfix: interfaceの引数の修正

--- domain/repository/userRepo.go
+++ domain/repository/userRepo.go
@@ -6,6 +6,6 @@ type UserRepository interface {
 	FindUserByUserID(userID entity.UserID) (*entity.User, error)
 	FindUserByAuthID(authID entity.AuthID) (*entity.User, error)
 	CreateUser(user entity.User) (*entity.User, error)
-	DeleteUser(userID entity.UserID) (*entity.User, error)
+	DeleteUser(user entity.User) (*entity.User, error)
 	UpdateUser(user entity.User) (*entity.User, error)
 }
