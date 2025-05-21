a759c28455deba9ae60d432f1dd0f455e17ac2ae
shooooooma415 <shoma20040415@icloud.com>
2025-05-21 22:50:08 +0900

    hotfix: ファイル名を修正

---
+++ usecase/fetchEventByGroupIDs.go
@@ -0,0 +1,35 @@
+package usecase
+
+import (
+	"chikokulympic-api/domain/entity"
+	"chikokulympic-api/domain/repository"
+	"fmt"
+)
+
+type FetchEventInfoUsecase interface {
+	Execute() (*entity.Event, error)
+}
+type FetchGroupInfoUsecaseImpl struct {
+	groupRepo repository.GroupRepository
+	eventRepo repository.EventRepository
+	eventID   *entity.EventID
+}
+
+func NewFetchEventInfoUsecase(groupRepo repository.GroupRepository, eventRepo repository.EventRepository, eventID *entity.EventID) *FetchGroupInfoUsecaseImpl {
+	return &FetchGroupInfoUsecaseImpl{
+		groupRepo: groupRepo,
+		eventRepo: eventRepo,
+		eventID:   eventID,
+	}
+}
+
+func (uc *FetchGroupInfoUsecaseImpl) Execute() (*entity.Event, error) {
+	event, err := uc.eventRepo.FindEventByEventID(*uc.eventID)
+	if err != nil {
+		return nil, fmt.Errorf("グループ情報の取得中にエラーが発生しました: %v", err)
+	}
+	if event == nil {
+		return nil, fmt.Errorf("指定されたイベントID %s が存在しません", *uc.eventID)
+	}
+	return event, nil
+}
