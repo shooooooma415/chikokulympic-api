b444584893829ab685830e571ebd28a5dffe8ce7
shooooooma415 <shoma20040415@icloud.com>
2025-05-21 18:55:43 +0900

    feat: fetchEventByGroupIDの実装

--- usecase/fetchEventByGroupID.go
+++ usecase/fetchEventByGroupID.go
@@ -1,1 +1,33 @@
 package usecase
+
+import (
+	"chikokulympic-api/domain/entity"
+	"chikokulympic-api/domain/repository"
+	"fmt"
+)
+
+type FetchEventInfoUsecase interface {
+	Execute(groupID entity.EventID) (*entity.Group, error)
+}
+type FetchGroupInfoUsecaseImpl struct {
+	groupRepo repository.GroupRepository
+	groupID   *entity.GroupID
+}
+
+func NewFetchEventInfoUsecase(groupRepo repository.GroupRepository, groupID *entity.GroupID) *FetchGroupInfoUsecaseImpl {
+	return &FetchGroupInfoUsecaseImpl{
+		groupRepo: groupRepo,
+		groupID:   groupID,
+	}
+}
+
+func (uc *FetchGroupInfoUsecaseImpl) Execute() (*entity.Group, error) {
+	group, err := uc.groupRepo.FindGroupByGroupID(*uc.groupID)
+	if err != nil {
+		return nil, fmt.Errorf("グループ情報の取得中にエラーが発生しました: %v", err)
+	}
+	if group == nil {
+		return nil, fmt.Errorf("指定されたグループID %s が存在しません", *uc.groupID)
+	}
+	return group, nil
+}
