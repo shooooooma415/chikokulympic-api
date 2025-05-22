6ed46f1a044c163e619deb3281eac838cf78bb4d
shooooooma415 <shoma20040415@icloud.com>
2025-05-22 15:45:10 +0900

    feat: responseモデルを追加

--- usecase/getArrivalRanking.go
+++ usecase/getArrivalRanking.go
@@ -1,1 +1,38 @@
 package usecase
+
+import (
+	"chikokulympic-api/domain/entity"
+	"chikokulympic-api/domain/repository"
+	"time"
+)
+type ArrivalRank struct {
+	Rank            int           `json:"rank"`
+	UserID          entity.UserID `json:"user_id"`
+	Name            string        `json:"name"`
+	Alias           entity.Alias  `json:"alias"`
+	ArrivalTime     time.Time     `json:"arrival_time"`
+}
+
+
+type ArrivalRanking struct {
+	EventID entity.EventID
+	Ranking   []ArrivalRank
+}
+
+type getArrivalRankingUseCase interface {
+	Execute() ([]ArrivalRanking, error)
+}
+
+type getArrivalRankingUseCaseImpl struct {
+	eventRepo repository.EventRepository
+	eventID   *entity.EventID
+	userRepo  repository.UserRepository
+}
+
+func NewGetArrivalRankingUseCase(eventRepo repository.EventRepository, eventID *entity.EventID, userRepo repository.UserRepository) *getArrivalRankingUseCaseImpl {
+	return &getArrivalRankingUseCaseImpl{
+		eventRepo: eventRepo,
+		eventID:   eventID,
+		userRepo:  userRepo,
+	}
+}
