c19269b6076a52703652648fa59f3205df2b7061
shooooooma415 <shoma20040415@icloud.com>
2025-06-03 10:58:31 +0900

    hotfix: リクエストの修正とレスポンスの追加

--- presentation/v1/postEvent.go
+++ presentation/v1/postEvent.go
@@ -3,6 +3,7 @@ package v1
 import "chikokulympic-api/domain/entity"
 
 type PostEventRequest struct {
+	GroupID              entity.GroupID          `json:"group_id" example:"group123"`
 	EventID              entity.EventID          `json:"event_id" example:"event123"`
 	EventTitle           entity.EventTitle       `json:"event_title" example:"テストイベント"`
 	EventDescription     entity.EventDescription `json:"event_description" example:"これはテストイベントです"`
@@ -15,5 +16,8 @@ type PostEventRequest struct {
 	EventStartDateTime   string                  `json:"event_start_date_time" example:"2023-10-01T10:00:00Z"`
 	EventEndDateTime     string                  `json:"event_end_date_time" example:"2023-10-01T12:00:00Z"`
 	EventClosingDateTime string                  `json:"event_closing_date_time" example:"2023-09-30T23:59:59Z"`
-	VotedMembers         []entity.VotedMember    `json:"voted_members" example:"[{\"is_arrival\": true, \"user_id\": \"user123\", \"vote\": \"yes\", \"arrival_date_time\": \"2023-10-01T09:00:00Z\"}]"`
+}
+
+type PostEventResponse struct {
+	EventID              entity.EventID          `json:"event_id" example:"event123"`
 }
