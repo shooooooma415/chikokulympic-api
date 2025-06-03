c22e965030fdc7390cd55128a0eb9631acb7c044
shooooooma415 <shoma20040415@icloud.com>
2025-06-03 11:34:24 +0900

    getEventsのHandlerを実装

--- presentation/v1/getEvents.go
+++ presentation/v1/getEvents.go
@@ -6,6 +6,7 @@ import (
 	"chikokulympic-api/usecase"
 	"fmt"
 	"net/http"
+	"strings"
 	"github.com/labstack/echo/v4"
 	"github.com/google/uuid"
 	"chikokulympic-api/middleware"
@@ -26,3 +27,42 @@ func NewGetEvents(eventRepo repository.EventRepository, groupRepo repository.Gro
 	}
 }
 
+func (g *GetEvents) Handler(c echo.Context) error {
+	groupIDsParam := c.QueryParam("group_ids")
+	if groupIDsParam == "" {
+		return c.JSON(http.StatusBadRequest, middleware.NewErrorResponse("グループIDは必須です"))
+	}
+
+	groupIDStrings := strings.Split(groupIDsParam, ",")
+	var groupIDs []entity.GroupID
+
+	for _, idStr := range groupIDStrings {
+		idStr = strings.TrimSpace(idStr)
+		if idStr == "" {
+			continue
+		}
+
+		// グループIDをUUIDに変換
+		groupUUID, err := uuid.Parse(idStr)
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, middleware.NewErrorResponse(fmt.Sprintf("無効なグループID: %s", idStr)))
+		}
+
+		groupIDs = append(groupIDs, entity.GroupID(groupUUID.String()))
+	}
+
+	if len(groupIDs) == 0 {
+		return c.JSON(http.StatusBadRequest, middleware.NewErrorResponse("有効なグループIDが指定されていません"))
+	}
+
+	events, err := usecase.NewFetchEventInfoUsecase(g.groupRepo, g.eventRepo, groupIDs).Execute()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, middleware.NewErrorResponse(fmt.Sprintf("イベント情報の取得中にエラーが発生しました: %v", err)))
+	}
+
+	response := GetEventsResponse{
+		Events: events,
+	}
+
+	return c.JSON(http.StatusOK, response)
+}
