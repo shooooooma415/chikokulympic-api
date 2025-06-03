527a2432a1eb3379a2673986b6393d311c78d218
shooooooma415 <shoma20040415@icloud.com>
2025-06-03 11:04:08 +0900

    hotfix: 修正

--- usecase/createEvent.go
+++ usecase/createEvent.go
@@ -11,16 +11,41 @@ type CreateEventUseCase interface {
 
 type CreateEventUseCaseImpl struct {
 	eventRepo repository.EventRepository
+	groupRepo repository.GroupRepository
 	event     *entity.Event
+	groupID   entity.GroupID
 }
 
-func NewCreateEventUseCase(eventRepo repository.EventRepository, event *entity.Event) *CreateEventUseCaseImpl {
+func NewCreateEventUseCase(eventRepo repository.EventRepository, groupRepo repository.GroupRepository, event *entity.Event, groupID entity.GroupID) *CreateEventUseCaseImpl {
 	return &CreateEventUseCaseImpl{
 		eventRepo: eventRepo,
+		groupRepo: groupRepo,
 		event:     event,
+		groupID:   groupID,
 	}
 }
 
 func (uc *CreateEventUseCaseImpl) Execute() (*entity.Event, error) {
-	return uc.eventRepo.CreateEvent(*uc.event)
+	// イベントを作成
+	createdEvent, err := uc.eventRepo.CreateEvent(*uc.event)
+	if err != nil {
+		return nil, err
+	}
+
+	// 指定したグループを取得
+	group, err := uc.groupRepo.FindGroupByGroupID(uc.groupID)
+	if err != nil {
+		return nil, err
+	}
+
+	// グループのイベントリストにイベントIDを追加
+	group.GroupEvents = append(group.GroupEvents, createdEvent.EventID)
+
+	// グループを更新
+	_, err = uc.groupRepo.UpdateGroup(*group)
+	if err != nil {
+		return nil, err
+	}
+
+	return createdEvent, nil
 }
