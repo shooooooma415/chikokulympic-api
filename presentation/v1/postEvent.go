841fa14b447e0651caaa3f5548f4a2bd191ffaa7
shooooooma415 <shoma20040415@icloud.com>
2025-06-03 10:53:27 +0900

    feat: PostEventのリクエストを定義

--- presentation/v1/postEvent.go
+++ presentation/v1/postEvent.go
@@ -1,1 +1,19 @@
 package v1
+
+import "chikokulympic-api/domain/entity"
+
+type PostEventRequest struct {
+	EventID              entity.EventID          `json:"event_id" example:"event123"`
+	EventTitle           entity.EventTitle       `json:"event_title" example:"テストイベント"`
+	EventDescription     entity.EventDescription `json:"event_description" example:"これはテストイベントです"`
+	EventLocationName    entity.LocationName     `json:"event_location_name" example:"東京ドーム"`
+	Cost                 entity.Cost             `json:"cost" example:"1000"`
+	EventMessage         entity.EventMessage     `json:"event_message" example:"参加してください！"`
+	EventAuthorID        entity.UserID           `json:"event_author_id" example:"user123"`
+	Latitude             entity.Latitude         `json:"latitude" example:"35.6895"`
+	Longitude            entity.Longitude        `json:"longitude" example:"139.6917"`
+	EventStartDateTime   string                  `json:"event_start_date_time" example:"2023-10-01T10:00:00Z"`
+	EventEndDateTime     string                  `json:"event_end_date_time" example:"2023-10-01T12:00:00Z"`
+	EventClosingDateTime string                  `json:"event_closing_date_time" example:"2023-09-30T23:59:59Z"`
+	VotedMembers         []entity.VotedMember    `json:"voted_members" example:"[{\"is_arrival\": true, \"user_id\": \"user123\", \"vote\": \"yes\", \"arrival_date_time\": \"2023-10-01T09:00:00Z\"}]"`
+}
