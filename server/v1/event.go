96edff9e0c0991d472fe719531a2c7851ff147a4
shooooooma415 <shoma20040415@icloud.com>
2025-06-27 23:27:12 +0900

    feat: server/v1/event.goの実装

---
+++ server/v1/event.go
@@ -0,0 +1,30 @@
+package v1
+
+import (
+	"chikokulympic-api/domain/repository"
+	presentationV1 "chikokulympic-api/presentation/v1"
+
+	"github.com/labstack/echo/v4"
+)
+
+type EventServer struct {
+	postEvent     *presentationV1.PostEvent
+	getEvents     *presentationV1.GetEvents
+	getEventBoard *presentationV1.GetEventBoard
+}
+
+func NewEventServer(eventRepo repository.EventRepository, groupRepo repository.GroupRepository, userRepo repository.UserRepository) *EventServer {
+	return &EventServer{
+		postEvent:     presentationV1.NewPostEvent(groupRepo, eventRepo),
+		getEvents:     presentationV1.NewGetEvents(eventRepo, groupRepo),
+		getEventBoard: presentationV1.NewGetEventBoard(groupRepo, eventRepo, userRepo),
+	}
+}
+
+func (s *EventServer) RegisterRoutes(e *echo.Echo) {
+	eventGroup := e.Group("/events")
+
+	eventGroup.POST("", s.postEvent.Handler)
+	eventGroup.GET("", s.getEvents.Handler)
+	eventGroup.GET("/board", s.getEventBoard.Handler)
+}
